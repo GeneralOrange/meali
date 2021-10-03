8a588dd63b0933488cf1cca7a5c07c8e71fc1db8
Erik van der Schaaf <erik@fuga.media>
2021-10-03 02:38:55 +0200

    Styling + first list attempt

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text } from 'react-native'
+import CreateNewList from '../components/createNewList'
+import Lists from '../components/Lists'
 import { globalStyles } from '../styles/global'
 
 const Home = ({ navigation }) => {
+    const DATA =
+        [
+            {
+                id: 'dsa',
+                title: 'Spareribs',
+            },
+            {
+                id: 'ccc',
+                title: 'Cheeseburger',
+            },
+            {
+                id: '123ssdsda',
+                title: 'Pannekoek'
+            }
+        ];
+
+    const [lists, setLists] = useState([]);
+    
+    setLists(DATA);
     return (
         <View style={globalStyles.container}>
-            <Text>Dit is de home screen</Text>
-            <Button onPress={()=> navigation.navigate('listDetail')} title='Naar listDetail'/>
+            {
+                lists.length < 1 && <Text style={globalStyles.titleText}>Er is nog geen lijst aangemaakt</Text>                
+            }
+            <Lists data={lists} navigation={navigation}/>
+            <CreateNewList navigation={navigation}/>
         </View>
     )
 }
