d48b5f619d7d2040c65662f83317ee7c8a855a71
General Orange <erik.17021995@gmail.com>
2021-10-15 01:34:30 +0200

    Implemented add button for lists

--- routes/defaultStack.js
+++ routes/defaultStack.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useFonts, Montserrat_400Regular } from '@expo-google-fonts/montserrat'
+import { AppLoading } from 'expo'
 import Home from '../screens/home'
 import listDetail from '../screens/listDetail' 
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
+
     return (
         <>
             <NavigationContainer>
-                <Stack.Navigator>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'regular',
+                            fontSize: 24
+                        },
+                        headerStyle: {
+                            backgroundColor: '#F07F19'
+                        }
+                    }}
+                    >
                     <Stack.Screen
                         name="Home"
                         component={ Home }
                         options={{
-                            title: ''
+                            title: 'Hi, meali-gebruiker!'
                         }}
                     />
                     <Stack.Screen
--- screens/home.js
+++ screens/home.js
@@ -5,32 +5,30 @@ import Lists from '../components/Lists'
 import { globalStyles } from '../styles/global'
 
 const Home = ({ navigation }) => {
-    const DATA =
-        [
-            {
-                id: 'dsa',
-                title: 'Spareribs',
-            },
-            {
-                id: 'ccc',
-                title: 'Cheeseburger',
-            },
-            {
-                id: '123ssdsda',
-                title: 'Pannekoek'
-            }
-        ];
+    const DATA = [];
 
-    const [lists, setLists] = useState([]);
+    const [lists, setLists] = useState(DATA);
+
+    const updateLists = (newTitle) => {
+        let newLists = [];
+        newLists.push({
+            id: newTitle,
+            title: newTitle
+        });
+
+        setLists([
+            ...lists,
+            ...newLists
+        ]);
+    }
     
-    setLists(DATA);
     return (
         <View style={globalStyles.container}>
             {
                 lists.length < 1 && <Text style={globalStyles.titleText}>Er is nog geen lijst aangemaakt</Text>                
             }
             <Lists data={lists} navigation={navigation}/>
-            <CreateNewList navigation={navigation}/>
+            <CreateNewList navigation={navigation} updateLists={updateLists} lists={lists}/>
         </View>
     )
 }
